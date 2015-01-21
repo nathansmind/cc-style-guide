d40ffc4b6d745d2bf9b05c84fa5e2c5c27207ea2
nathansmind <nathansmind@gmail.com>
2015-01-21 09:46:12 -0700

    + index

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Design Server</title>
+	<link rel="stylesheet" href="css/style.css">	
+	<script src="js/script.js"></script>
+<!-- 	<link rel="stylesheet" href="http://basehold.it/24"> -->
+</head>
+<body class="pdp home">
+	
+	<?php include 'inc/cc-header.php'; ?>
+	
+	<div class="plp-top-targeter" style="background-image: url('http://placehold.it/1600x470/ccc/999');">
+		<span class="icon-cc-icon_arrow-left"></span>
+		<ul class="slider-bullets">
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ul>
+		<span class="icon-cc-icon_arrow-right"></span>
+	</div>
+	
+	<div class="brands">
+		<ul>
+			<li><a href="">Santa Cruz Bicycles</a></li>
+			<li><a href="">Assos</a></li>
+			<li><a href="">Yeti Cycles</a></li>
+			<li><a href="">Castelli</a></li>
+			<li><a href="">Ridley</a></li>
+			<li><a href="" class="btn-flat secondary">More brands</a></li>
+		</ul>
+	</div>
+	
+	<div class="a-b-targeters">
+		<img src="http://placehold.it/480x220/ccc/999">
+		<img src="http://placehold.it/480x220/ccc/999">
+	</div>
+	
+	<div class="storyboard">
+
+		<img src="http://placehold.it/980x580/ccc/999">
+		
+	</div>
+
+
+
+
+
+
+
+
+	
+	<?php include 'inc/cc-footer.php'; ?>
